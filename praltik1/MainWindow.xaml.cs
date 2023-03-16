de1d4ec1c5a173c500d83e97130955399d643c28
rof1anbtw <113422300+rof1anbtw@users.noreply.github.com>
2023-03-16 17:41:25 +0300

    Add files via upload

---
+++ praltik1/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using praltik1.NewPraktik1DataSetTableAdapters;
+
+namespace praltik1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        заказыTableAdapter zakaz = new заказыTableAdapter();
+        пользователиTableAdapter polz = new пользователиTableAdapter();
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+            Zakaz.ItemsSource = zakaz.GetData();
+            ComboBox.ItemsSource = polz.GetData();
+            ComboBox.DisplayMemberPath = "имя";
+
+        }
+
+
+        private void ComboBox_S(object sender, SelectionChangedEventArgs e)
+        {
+            object cell = (ComboBox.SelectedItem as DataRowView).Row[0];
+            MessageBox.Show(cell.ToString());
+        }
+    }
+}
